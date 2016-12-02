92fa5372e4466cb0c8347e7840ab82c9a181bffc
Dugagjin <lashi.dugagjin@gmail.com>
2016-12-02 19:47:40 +0100

    try to save compressed file

--- codec.js
+++ codec.js
@@ -9,29 +9,33 @@ stream.on('readable', () => {
         str += String.fromCharCode(parseInt(chunk.toString('hex'), 16));
     }
 });
-stream.on('end', () => {
-    let comp = encode(str);
-    let decomp = decode(comp);
-    console.log('input: ' + str.length +'\tcompressed: '+ comp.length + '\tdecompressed: ' + decomp.length);
-    fs.writeFileSync('output.wav', decomp, 'binary');
+stream.on('finish',() => {
+    //const x = [206182, 206044, 206132, 206107, 206130, 206188, 204479, 201072, 206158, 206150, 206137, 206148, 206098, 193595, 205980, 197932, 206105];
+    const comp = encode(str);
+    const toDecode = [];
+    function hex16(val) {
+        val &= 0xFFFFFF;
+        const hex = val.toString(16).toUpperCase();
+        return ("000000" + hex).slice(-6);
+    }
+    const wStream = fs.createWriteStream('t.txt');
+    comp.forEach(int => {
+        const buf = Buffer.from(hex16(int), 'hex');
+        wStream.write(buf)
+    })
+    wStream.end()
+    wStream.on('finish', readBinaryFile)
+    function readBinaryFile() {
+        const stream2 = fs.createReadStream('t.txt');
+        stream2.on('readable', () => {
+            let chunk2;
+            while (chunk2 = stream2.read(3)) {
+                toDecode.push(chunk2.readUIntBE(0, 3))
+            }
+        });
+        stream2.on('end', () => {
+            console.log(toDecode);
+            fs.writeFileSync('output.wav', decode(toDecode), 'binary');
+        })
+    }
 });
-
-
-/*
-stream.on('end', () => {
-    comp = encode(str);
-    let toSave = comp.map((e) => {
-        return String.fromCharCode(e);
-    }).join('');
-
-    fs.writeFileSync('compressed.txt', toSave, 'utf8');
-    let readed = fs.readFileSync('compressed.txt', 'utf8');
-
-    let toDecode = readed.split('').map((e) => {
-        return e.charCodeAt(e);
-    });
-
-    let decomp = decode(toDecode);
-    fs.writeFileSync('decompressed.wav', decomp, 'binary');
-});
-*/
