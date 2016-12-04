35da44d12f4969cbe7a5b952bd6310654cd36f19
Dugagjin Lashi <lashi.dugagjin@gmail.com>
2016-12-04 15:51:56 +0100

    better code

--- codec.js
+++ codec.js
@@ -1,34 +1,22 @@
 const fs = require("fs");
 require('./algorithm.js')();
 
-let str = '';
-const stream = fs.createReadStream('Startup.wav');
-stream.on('readable', () => {
-    let chunk;
-    while (chunk = stream.read(1)) {
-        str += String.fromCharCode(parseInt(chunk.toString('hex'), 16));
-    }
-    let comp = encode(str);
-    let decomp = decode(comp);
-    console.log('input: ' + str.length +'\tcompressed: '+ comp.length + '\tdecompressed: ' + decomp.length);
-    fs.writeFileSync('output.wav', decomp, 'binary');
+let str = fs.readFileSync('Startup.wav', 'binary');
+let comp = encode(str);
+let compUTF8 = comp.map((e) => {
+    return String.fromCharCode(e);
+}).join('');
+
+fs.writeFileSync('compressed.txt', compUTF8, 'binary');
+let readed = fs.readFileSync('compressed.txt', 'binary');
+
+let decomp = decode(comp);
+let toSave = decomp.split('').map((e) => {
+    return e.charCodeAt(e);
 });
 
-/*
-stream.on('end', () => {
-    comp = encode(str);
-    let toSave = comp.map((e) => {
-        return String.fromCharCode(e);
-    }).join('');
+fs.writeFileSync('output.wav', decomp, 'binary')
 
-    fs.writeFileSync('compressed.txt', toSave, 'utf8');
-    let readed = fs.readFileSync('compressed.txt', 'utf8');
-
-    let toDecode = readed.split('').map((e) => {
-        return e.charCodeAt(e);
-    });
-
-    let decomp = decode(toDecode);
-    fs.writeFileSync('decompressed.wav', decomp, 'binary');
-});
-*/
+console.log('original: \t' + Buffer.byteLength(str))
+console.log('compressed: \t' + Buffer.byteLength(compUTF8))
+console.log('output: \t' + Buffer.byteLength(decomp))
