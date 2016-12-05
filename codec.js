f7f405eb9c6a18da5d05f440fd1dde7845033a93
Ryan Megidov <ryan@megidov.com>
2016-12-05 15:14:42 +0100

    Initial commit

--- codec.js
+++ codec.js
@@ -1,20 +1,50 @@
-const fs = require("fs")
-const path = require("path")
+const fs = require('fs')
+const path = require('path')
+const crypto = require('crypto')
 const lzw = require('./lzw')
 
-//const input = path.join(__dirname, 'input.wav') // smaller file for development
-const input = path.join(__dirname, 'original.wav') // original wav
+const input = path.join(__dirname, 'input.wav') // smaller file for development
+//const input = path.join(__dirname, 'original.wav') // original wav
 const output = path.join(__dirname, 'output.txt')
+//-----------------------------------------------------
 
-const content = fs.readFileSync(input, 'binary')
-const encoded = lzw.encode(content);
+const content = fs.readFileSync(input, null)
+const encoded = lzw.encode(content, 'utf8');
+const stream  = fs.createWriteStream(output)
 
-console.log('Before enconding:', content.length)
-console.log('After enconding:', encoded.length)
+stream.write(encoded)
+stream.end()
+stream.on('finish', () => {
+    const readBytes = fs.readFileSync(output)
+    const decoded = lzw.decode(Buffer.from(readBytes))
 
-fs.writeFileSync(output, encoded, 'binary');
+    console.log('content:', Buffer.byteLength(content), content)
+    console.log('encoded:', Buffer.byteLength(encoded), encoded)
+    console.log('decoded:', Buffer.byteLength(decoded), decoded)
 
-const decoded = lzw.decode(encoded);
-console.log('After deconding:', decoded.length)
+    if (checksum(encoded) !== checksum(readBytes)) {
+        throw new Error('Checksum failed, we are not writing the files correctly')
+    }
 
-fs.writeFileSync('output.wav', decoded, 'binary')
+    console.log('Hash (original):\t', checksum(content))
+    console.log('Hash  (decoded):\t', checksum(decoded))
+
+    //Buffer.dif
+
+    //console.log('Hash (input.wav):\t', checksum(content))
+    //console.log('Hash (output.wav):\t', checksum(decoded))
+    /*console.log('Content (input.wav):\t', content)
+    console.log('Content (output.wav):\t', decoded)
+    console.log('Before enconding:', content.length)
+    console.log('After enconding:', encoded.length)
+    console.log('After decoding:', decoded.length)*/
+})
+
+
+
+//fs.writeFileSync('output.wav', decoded, 'binary')
+
+// Helper for getting md5 hash
+function checksum(str, algorithm, encoding) {
+    return crypto.createHash(algorithm || 'md5').update(str, 'utf8').digest(encoding || 'hex')
+}
--- lzw.js
+++ lzw.js
@@ -1,57 +1,103 @@
-const LZW = function LZW() {}
+/*
+ lzw - Lempel–Ziv–Welch data compression algorithm
+ copyright (c) 2013 Yaroslav Gaponov <yaroslav.gaponov@gmail.com>
+*/
 
-LZW.prototype.encode = function(s) {
-    const dict = {};
-    const data = (s + '').split('');
-    const out = [];
+function byte2hex(val) {
+    if (val < 16) return '0' + val.toString(16);
+    return val.toString(16);
+}
 
-    let currChar;
-    let phrase = data[0];
-    let code = 256;
+function hex2byte(hex) {
+    let bytes = [];
+    for (let i = 0; i < hex.length; i += 2) {
+        bytes.push(parseInt(hex.substr(i, 2), 16));
+    }
+    return bytes;
+}
 
-    for (let i = 1; i < data.length; i++) {
-        currChar = data[i];
-        if (dict[phrase + currChar] != null) {
-            phrase += currChar;
+module.exports.encode = function(src) {
+
+    if (!Buffer.isBuffer(src)) {
+        throw new Error('Encode: input must be a buffer');
+    }
+
+    let abc = {}, size = 256;
+    for (let i = 0; i < size; i++) {
+        abc[byte2hex(i)] = i;
+    }
+
+    let acc = '', offset = 0, buf = [], next_byte;
+    while (offset < src.length) {
+        next_byte = byte2hex(src.readUInt8(offset));
+        if (!abc[acc + next_byte]) {
+
+            let bbb = abc[acc] << 12 | abc[next_byte];
+            buf.push(bbb >> 16);
+            buf.push((bbb >> 8) & 0xff);
+            buf.push(bbb & 0xff);
+
+            if (size > 0xfff) size = 256;
+
+            abc[acc + next_byte] = size++;
+            acc = '';
+
+        } else {
+            acc += next_byte;
         }
-        else {
-            out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-            dict[phrase + currChar] = code;
-            code++;
-            phrase=currChar;
+        offset++;
+    }
+    if (acc != '') {
+        buf.push(abc[acc] >> 4);
+        buf.push((abc[acc] & 0x0f) << 4);
+    }
+
+    return new Buffer(buf);
+}
+
+module.exports.decode = function(src) {
+
+    if (!Buffer.isBuffer(src)) {
+        throw new Error('Decode: input must be a buffer');
+    }
+
+    let abc1 = {}, abc2 = {}, size = 256;
+    for (let i = 0; i < size; i++) {
+        abc1[byte2hex(i)] = i;
+        abc2[i] = byte2hex(i);
+    }
+
+    let offset = 0, acc = '', buf = [], idx;
+
+    loop: {
+        while (offset < src.length) {
+
+            for (let serialNumber = 0; serialNumber < 2; serialNumber++) {
+
+                if (serialNumber) {
+                    if ((offset + 2) >= src.length) break loop;
+                    else idx = ((src.readUInt8(offset + 1) & 0x0f) << 4) | src.readUInt8(offset + 2);
+                } else {
+                    if ((offset + 1) >= src.length) break loop;
+                    else idx = (src.readUInt8(offset) << 4) | (src.readUInt8(offset + 1) >> 4)
+                }
+
+                buf = buf.concat(hex2byte(abc2[idx]));
+
+                acc += abc2[idx];
+                if (!abc1[acc]) {
+                    if (size > 0xfff) size = 256;
+                    abc1[acc] = size;
+                    abc2[size] = acc;
+                    size++;
+                    acc = '';
+                }
+
+            }
+
+            offset += 3;
         }
     }
-    out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-    for (let i = 0; i < out.length; i++) {
-        out[i] = String.fromCharCode(out[i]);
-    }
-    return out.join('');
-};
 
-LZW.prototype.decode = function(s) {
-    let dict = {};
-    let data = (s + "").split("");
-    let currChar = data[0];
-    let oldPhrase = currChar;
-    let out = [currChar];
-    let code = 256;
-    let phrase;
-
-    for (let i = 1; i < data.length; i++) {
-        let currCode = data[i].charCodeAt(0);
-        if (currCode < 256) {
-            phrase = data[i];
-        }
-        else {
-            phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
-        }
-        out.push(phrase);
-        currChar = phrase.charAt(0);
-        dict[code] = oldPhrase + currChar;
-        code++;
-        oldPhrase = phrase;
-    }
-    return out.join('');
-};
-
-module.exports = new LZW();
+    return new Buffer(buf);
+}
